d1e34a8c13579686d1ea64ee41459c837f357aae
Mauro2 <mauro.gandelli@gmail.com>
2010-01-21 14:24:58 -0300

    Cambios de planteo. No compila

--- TP2.pas
+++ TP2.pas
@@ -16,7 +16,7 @@ const
 type
 	TD_CodZoo = string[7]; { 8 bytes }
 	TD_CodEsp = longint;
-	TD_MensajeError = string[100];
+	TD_Alert = string[100];
 	
 	TVec_CodEspExt = array[SubIndInf..SubIndSup] of TD_CodEsp;
 	
@@ -30,7 +30,7 @@ type
 	
 	TA_Zoomundo = file of TR_Zoomundo;
 	
-	{Declaración de Lista Zoologico}
+	{Declaracion de Lista Zoologico}
 	TL_Zoo = TN_Zoo;
 	
 	TR_Zoo = record
@@ -49,7 +49,7 @@ type
 	
 	
 	
-procedure MostrarError( mensaje: TD_MensajeError );
+procedure alert( mensaje: TD_Alert );
 begin
 	writeln( mensaje );
 end;
@@ -66,22 +66,32 @@ end;
 
 procedure DesplazarElementos( var vector: TVec_CodEspExt;
 							  corriente: integer;
-							  var cantElem: integer );
+							  var cantElem: byte );
 var
 	i: integer;
 	aux: TD_CodEsp;
 begin
 	
-	for i := corriente to cantElem do
+	{TODO: Revisar}
+	aux := vector[ corriente ];
+	writeln( aux );
+	
+	
+	for i := (corriente + 1) to cantElem do
 	begin
+		vector[i] := aux;
 		aux := vector[i + 1];
-		vector[i + 1] := vector[i];
+	end;
+	
+	for i := 1 to cantElem do
+	begin
+		write( vector[i], ' - ' );
 	end;
 	
 	inc(cantElem);
-	
 end;
 
+{TODO: DEPRECATE, no es necesario que esten en orden en el vector.
 {pre-condicion: Vector inicializado con InicializarVector}
 {post-condicion: Inserta en el vector}
 procedure InsertarVector( var vector: TVec_CodEspExt;
@@ -93,21 +103,28 @@ var
 begin
 	if cantElem <= SubIndSup  then
 	begin
+		corriente := 1;
+		
 		while ( item > vector[corriente]) and (corriente < cantElem ) do
 		begin
 			inc(corriente);
-		end
+		end;
+		
+		writeln( corriente );
+		
+		DesplazarElementos( vector, corriente, cantElem );
+		{vector[corriente] := item;}
 	end
 	else
 	begin
-		MostrarError( 'Vector lleno, no se pueden insertar mas registros' );
+		alert( 'Vector lleno, no se pueden insertar mas registros' );
 	end;
 end;
 
 {TODO: Hacer Procedimiento}
-procedure InicializarVariables( );
+procedure InicializarVariables( var cantElem: byte );
 begin
-	writeln( 'NotImplemented' );
+	cantElem := 0;
 end;
 
 function EsEspecieEnExtincion( codEsp: TD_CodEsp ) : boolean;
@@ -120,31 +137,30 @@ procedure ProcesarRegistro( item: TR_Zoomundo;
 							var lista: TL_Esp;
 							var cantElem: byte );
 begin
-
     if EsEspecieEnExtincion( item.CodEsp ) then
 	begin
-		
-		if( item.CodZoo = ZooBSAS ) then
+		if item.CodZoo = ZooBSAS then
 		begin
 			InsertarVector( vector, item.CodEsp, cantElem );
 		end
 		else
 		begin
-			{InsertarLista( lista, item );}
+			InsertarLista( lista, item );
 		end;
-		
 	end;
 end;
 
 {TODO: pre-condicion:}
-{post-condicion: Recorre el archivo secuencialmente y genera los indices de especies en extincion en 
-		 		 el zoologico de BsAs y los zoologicos en donde se encuentran}
+{post-condicion: Recorre el archivo secuencialmente y genera un vector de especies en extincion 
+				 ya existentes en el zoologico de BsAs y una lista de listas con todas las especies en extincion
+				 y los zoologicos en donde se encuentran (excepto en Buenos Aires)}
 procedure CrearIndices( var arch: TA_Zoomundo;
 						var vector: TVec_CodEspExt;
 						var	lista: TL_Esp;
 						var	cantElem: byte );
 var
 	auxRec: TR_Zoomundo;
+	
 begin
 	reset( arch );
 	read( arch, auxRec );
@@ -152,6 +168,7 @@ begin
 	while not eof( arch ) do
 	begin		
 		ProcesarRegistro( auxRec, vector, lista, cantElem );
+		
 		read( arch, auxRec );
 	end;
 	
@@ -160,7 +177,14 @@ begin
 	close( arch );
 end;
 
-	
+{pre-condicion: Tener las listas con datos de CrearIndices}
+{post-condicion: Busca en las listas que contienen todas las especies en extincion
+				 los animales que no estan en el Zoologico de Buenos Aires}
+procedure Proceso( )
+begin
+	writeln( 'Not Implemented' );
+end;
+
 var
 	VectorEsp: TVec_CodEspExt;
 	ListaEsp: TL_Esp;
@@ -172,8 +196,9 @@ var
 begin
 	assign( Archivo, RutaArchivo );
 	
-	InicializarVariables( );
+	InicializarVariables( CantElem );
 	CrearIndices( Archivo, VectorEsp, ListaEsp, CantElem );
+	Proceso( );
 	
 	writeln( '*** Fin del Programa ***' );
 end.
